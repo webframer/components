66b95f4066ccbe9d1bc5538167d650f456c6d8df
Chris Co <coswarm@gmail.com>
2022-07-13 17:04:47 +0300

    improve components

--- View.svelte
+++ View.svelte
@@ -1,1 +1,12 @@
-<div class:col={true} {...$$props}><slot/></div>
+<script>
+  import { cn } from 'utils-pack/components'
+
+  export let fill, reverse, rtl, left, right, top, bottom, center, middle
+  let className
+  export { className as class }
+</script>
+<div class={cn(className, 'col', {
+  fill, reverse, rtl, left, right, top, bottom, center, middle, pointer: $$props.onClick})
+  } {...$$restProps}>
+  <slot/>
+</div>
