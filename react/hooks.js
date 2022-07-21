af5677d7a507b8c3f7f6d782fae296318f4c7553
Chris Co <coswarm@gmail.com>
2022-07-21 16:29:56 +0300

    useAnimatedHeight hook

--- react/hooks.js
+++ react/hooks.js
@@ -1,4 +1,5 @@
-import { useRef, useState } from 'react'
+import { isEqualJSON } from '@webframer/utils'
+import { useEffect, useRef, useState } from 'react'
 
 /**
  * React Hook to simulate Class Component behaviors with `this` instance.
@@ -19,4 +20,45 @@ export function useInstance (initialState = {}) {
     instance.forceUpdate = () => setState(state => ({...state}))
   }
   return instance
+}
+
+/**
+ * Animate height for React Function Component
+ * @example:
+ *    function Component (props) {
+ *      const style = useAnimatedHeight(props.style)
+ *      return <View style={style}>...</View>
+ *    }
+ *
+ * @param {{height: number | string, transition?: string}} style - component's CSS style
+ * @param {number} [duration] - animation duration
+ * @returns {object} style - to apply to CSS
+ */
+export function useAnimatedHeight (style, duration = 500) {
+  const {height, transition} = style
+  const [_style, setStyle] = useState(style)
+
+  // Animate transition when height changes
+  useEffect(() => {
+    if (_style === style) return
+
+    // No height change,
+    if (_style.height === height) {
+      // but other styles changed
+      if (!isEqualJSON(_style, style)) setStyle(style)
+      return
+    }
+
+    // Height changed
+    const _transition = `height ${duration}ms`
+    setStyle({
+      ...style,
+      transition: transition ? [transition, _transition].join(', ') : _transition,
+    })
+
+    // Clean up transition
+    const timer = setTimeout(() => setStyle(style), duration)
+    return () => clearTimeout(timer)
+  }, [height, style, duration])
+  return _style
 }
