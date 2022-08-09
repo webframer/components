53eab78caeb3a2a455848782377bd01631194bee
Chris Co <coswarm@gmail.com>
2022-08-09 14:53:54 +0300

    element utils

--- utils.js
+++ utils.js
@@ -1,2 +1,3 @@
 export * from './utils/css.js'
+export * from './utils/element.js'
 export * from './utils/interactions.js'
---
+++ utils/element.js
@@ -0,0 +1,64 @@
+/**
+ * Make DOM element take no space
+ * @param {object|HTMLElement} node - to collapse
+ */
+export function collapseElement (node) {
+  node.style.width = '0'
+  node.style.height = '0'
+  node.style.minWidth = '0'
+  node.style.minHeight = '0'
+  node.style.marginTop = '0'
+  node.style.marginBottom = '0'
+  node.style.marginLeft = '0'
+  node.style.marginRight = '0'
+  node.style.paddingTop = '0'
+  node.style.paddingBottom = '0'
+  node.style.paddingLeft = '0'
+  node.style.paddingRight = '0'
+}
+
+/**
+ * Get index of current Node element within its direct parent element
+ * @param {object|HTMLElement} node
+ * @returns {number} index
+ */
+export function indexOfElement (node) {
+  return [...node.parentElement.children].indexOf(node)
+}
+
+/**
+ * Place a node element at given index inside a parent node element
+ * @param {object|HTMLElement} parent
+ * @param {number} index - to place at
+ * @param {object|HTMLElement} node - to place
+ * @returns {object|HTMLElement}
+ */
+export function placeAtIndex (parent, index, node) {
+  return parent.insertBefore(node, parent.children[index])
+}
+
+/**
+ * Get Computed CSS property to animate for inserting/removing child nodes
+ * @param {object|HTMLElement} node - the parent element to get property from
+ * @returns {string} side - can be 'width' or 'height'
+ */
+export function sideToAnimate (node) {
+  // set default in case the node does not have flex layout
+  let side = 'height'
+  let css = getComputedStyle(node)
+  let display = css.getPropertyValue('display')
+  if (display === 'flex' || display === 'inline-flex') {
+    switch (css.getPropertyValue('flex-direction')) {
+      case 'row':
+      case 'row-reverse':
+        side = 'width'
+        break
+      case 'column-reverse':
+      case 'column':
+      default:
+        side = 'height'
+    }
+  }
+  css = null // garbage clean
+  return side
+}
