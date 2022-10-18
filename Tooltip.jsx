53f6681e9f304b0c20b3be9fe8180a6db41e0ec4
Chris Co <coswarm@gmail.com>
2022-10-18 23:28:37 +0300

    refactor Tooltip with fixed positioning for scroll

--- Tooltip.jsx
+++ Tooltip.jsx
@@ -8,40 +8,40 @@ import {
   unsubscribeFrom,
 } from '@webframer/js'
 import cn from 'classnames'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useLayoutEffect, useRef } from 'react'
 import { useInstance } from './react/hooks.js'
 import { renderProp } from './react/render.js'
 import { type } from './types.js'
 
 /**
- * todo: fix Tooltip incorrect position after scroll
- * todo: component improvement 3 - align tooltip
+ * todo: component improvement 3 - align tooltip + RTL position support
  * Tooltip Component
  * @notes:
- *  - Offset position should use margin to allow hovering over Tooltip
+ *  - Offset position should use margin to allow hovering over Tooltip, without loosing mouse hover
  */
 export function Tooltip ({
   position, on, open, delay, animation, theme,
   align = (position === 'left' || position === 'right') ? 'middle' : 'center',
-  className, row, col = !(row), fill, reverse, rtl,
+  className, style, row, col = !(row), fill, reverse, rtl,
   left, right, top, bottom, center, middle,
   children, ...props
 }) {
   const ref = useRef(null)
-  const [self, state] = useInstance({open, position})
+  const [self, state] = useInstance({position}) // initially always closed to prerender
   const prerender = state.prerender
-  open = state.open
-  position = state.position
+  self.props = {open, position}
 
   // Event Handlers --------------------------------------------------------------------------------
   if (!self.open) {
     self.open = () => {
-      if (self.canceled) return
-      self.setState({prerender: true}) // first render to compute dimensions for positioning
-      // the actual opening is called within useEffect to ensure the prerender has finished
+      if (self.canceled || self.state.open) return
+      self.setState({prerender: true, style: {bottom: 0, right: 0}})
+      // ^ first prerender to compute dimensions for positioning
+      // (must have bottom + right positions set to work within scroll).
+      // The actual opening is called within useLayoutEffect to ensure the prerender has finished.
     }
     self.close = () => {
-      if (self.state.open) self.setState({open: false, prerender: false})
+      if (self.state.open) self.setState({open: false, prerender: false, style: null})
     }
     self.openDelayed = debounce(self.open, delay)
     self.openOnHover = () => {
@@ -101,46 +101,77 @@ export function Tooltip ({
   }, [on])
   useEffect(() => {
     self.mounted = true
-    if (open && ref.current) self.open() // Show Tooltip on mount
+    if (self.props.open) self.open() // Show Tooltip on mount
   }, [])
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (!prerender || !ref.current) return
-    self.setState({prerender: false, open: true, position: positionFrom(ref.current, position)})
-  }, [prerender, position])
+    self.setState({prerender: false, open: true, ...positionFrom(ref.current, self.props.position)})
+  }, [prerender])
 
-  return ( // Outer div is required to take full size of the parent element for positioning
-    <span className={cn(
-      'tooltip row', `theme-${theme}`,
-      !prerender && `from-${position} ${alignClass(align, position)} position-fill`,
-    )} ref={ref} style={prerender ? styleFixed : noPointerEvents}>
+  // Render Props ----------------------------------------------------------------------------------
+  const shouldRender = self.mounted && (prerender || state.open)
+  if (state.style) style = style ? {...style, ...state.style} : state.style
+  position = state.position
+  open = state.open
+
+  return (
+    <span className={cn('tooltip col position-fixed', `theme-${theme}`,
+      !prerender && `tooltip-${position} tooltip-${align}`,
+      open ? 'pointer-events-auto z-10' : 'pointer-events-none', {
+        'hidden': !shouldRender,
+        'invisible': prerender, // tailwind only recognizes text literal
+        [animation]: open && !prerender,
+      })} style={style} ref={ref}>
       {/**
-       This extra middle div fixes {width/height: fit-content} problem for Safari and serves
-       as the container to retain hover state when user wants to hover over the tooltip.
-       Offset position is created by adding padding to this div via tooltip.less file.
-       @note: 'position-fixed', instead of 'absolute' is required for 'from-right' CSS to work,
-       otherwise, text collapses in width due to overflow.
-       => setting 'position-fixed' without top/left/right/bottom behaves like position absolute
-       (i.e. relative to the parent element), but allows z-index to override ancestors' siblings
-       => However, position fixed does not work for scrolled elements, it stays in the original
-       position even if the parent element has moved.
+       * Inner div behaves like View.jsx - tooltip pointer can be added to this
+       * Skip rendering content when closed for performance,
+       * but the outer ref is required to register events.
        */
-        self.mounted && (prerender || open) && // Skip rendering when closed for performance
-        <span className={cn('col', {'position-fixed': !prerender})}
-              style={open ? styleTooltip : noPointerEvents}>
-        {/* Inner div behaves like View.jsx - tooltip pointer can be added to this */}
-          <span className={cn(className, 'tooltip__content', {
-            col, row, fill, reverse, rtl,
-            left, right, top, bottom, center, middle,
-            'pointer': props.onClick,
-            'invisible': prerender, // tailwind only recognizes text literal
-            [animation]: open && !prerender,
-          })} {...props}>
+        shouldRender &&
+        <span className={cn(className, 'tooltip__content', {
+          col, row, fill, reverse, rtl, left, right, top, bottom, center, middle,
+          'pointer': props.onClick,
+        })} {...props}>
           {renderProp(children, self)}
         </span>
-      </span>
       }
     </span>
   )
+
+  // Archived version: using absolute position
+  // return ( // Outer div is required to take full size of the parent element for positioning
+  //   <span className={cn(
+  //     'tooltip row', `theme-${theme}`,
+  //     !prerender && `from-${position} ${alignClass(align, position)} position-fill`,
+  //   )} ref={ref} style={prerender ? styleFixed : noPointerEvents}>
+  //     {/**
+  //      This extra middle div fixes {width/height: fit-content} problem for Safari and serves
+  //      as the container to retain hover state when user wants to hover over the tooltip.
+  //      Offset position is created by adding padding to this div via tooltip.less file.
+  //      @note: 'position-fixed', instead of 'absolute' is required for 'from-right' CSS to work,
+  //      otherwise, text collapses in width due to overflow.
+  //      => setting 'position-fixed' without top/left/right/bottom behaves like position absolute
+  //      (i.e. relative to the parent element), but allows z-index to override ancestors' siblings
+  //      => However, position fixed does not work for scrolled elements, it stays in the original
+  //      position even if the parent element has moved.
+  //      */
+  //       self.mounted && (prerender || open) && // Skip rendering when closed for performance
+  //       <span className={cn('col', {'position-fixed': !prerender})}
+  //             style={open ? styleTooltip : noPointerEvents}>
+  //       {/* Inner div behaves like View.jsx - tooltip pointer can be added to this */}
+  //         <span className={cn(className, 'tooltip__content', {
+  //           col, row, fill, reverse, rtl,
+  //           left, right, top, bottom, center, middle,
+  //           'pointer': props.onClick,
+  //           'invisible': prerender, // tailwind only recognizes text literal
+  //           [animation]: open && !prerender,
+  //         })} {...props}>
+  //         {renderProp(children, self)}
+  //       </span>
+  //     </span>
+  //     }
+  //   </span>
+  // )
 }
 
 Tooltip.propTypes = {
@@ -159,7 +190,6 @@ Tooltip.defaultProps = {
   animation: 'fade-in',
   delay: 1000,
   on: 'hover,click',
-  open: false,
   position: 'top',
   theme: 'dark',
   role: 'tooltip',
@@ -202,11 +232,6 @@ export function useTooltip (props, defaultProps) {
   return [<ToolTip {...tooltip} />]
 }
 
-// Render Tooltip as fixed position initially to measure its desired width/height
-const styleFixed = {position: 'fixed', right: 0, bottom: 0, visibility: 'hidden'}
-const styleTooltip = {zIndex: 99, pointerEvents: 'auto'}
-const noPointerEvents = {pointerEvents: 'none'}
-
 // Convert Tooltip `align` props to flex layout alignment CSS className
 function alignClass (align, position) {
   switch (position) {
@@ -240,19 +265,38 @@ function alignClass (align, position) {
  * @param {Element} node - the Tooltip outer div element
  * @param {string} position - desired initially
  * @param {number} [offset] - to account for Tooltip offset position and pointer
- * @returns {string} position - for optimum placement
+ * @returns {{position: string, style: object}} state - props for optimum placement
  */
 function positionFrom (node, position, offset = 15) {
-  // The middle div can have 0 height if Tooltip children is an inline element.
-  // Use inner div to guarantee correct Tooltip dimensions.
-  let {offsetHeight, offsetWidth} = node.children[0].children[0]
-  const {top, left, width, height} = node.parentElement.getBoundingClientRect()
+  // The outer div can have 0 height if Tooltip children is an inline element.
+  // Use .tooltip__content div to guarantee correct Tooltip dimensions.
+  // offsetHeight/offsetWidth dimensions may be incorrect within scroll, make sure the outer div has
+  // fixed bottom and right positions set to 0 during the prerender phase.
+  const {offsetHeight, offsetWidth} = node.children[0]
+  const {top, left, bottom, right, width, height} = node.parentElement.getBoundingClientRect()
   const {innerWidth, innerHeight} = window
-  const bottom = innerHeight - Math.max(0, top + height) // available space from the edge of viewport
-  const right = innerWidth - Math.max(0, left + width) // available space from the edge of viewport
+  const _bottom = innerHeight - Math.max(0, top + height) // available space from the edge of viewport
+  const _right = innerWidth - Math.max(0, left + width) // available space from the edge of viewport
+  position = positionComputed(position, top, _bottom, left, _right, offsetWidth, offsetHeight, offset)
+
+  // Besides the position direction, the tooltip also needs coordinates along the other axis.
+  // By default, try to place it in the center/middle, then restrict to be within viewport.
+  switch (position) {
+    case 'bottom':
+      return {position, style: {top: bottom, left: Math.max(0, left + width / 2 - offsetWidth / 2)}}
+    case 'left':
+      return {position, style: {right: innerWidth - left, top: Math.max(0, top + height / 2 - offsetHeight / 2)}}
+    case 'right':
+      return {position, style: {left: right, top: Math.max(0, top + height / 2 - offsetHeight / 2)}}
+    case 'top':
+    default:
+      return {position, style: {bottom: innerHeight - top, left: Math.max(0, left + width / 2 - offsetWidth / 2)}}
+  }
+}
+
+function positionComputed (position, top, bottom, left, right, offsetWidth, offsetHeight, offset) {
   offsetHeight += offset
   offsetWidth += offset
-
   // The logic can get very complicated if we try to always render the Tooltip within viewport.
   // Instead, only check if enough space is available for given position direction.
   // For example: if not enough top space, try to position to the bottom, else right/left, or revert.
