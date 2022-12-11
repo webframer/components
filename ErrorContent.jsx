9b63ac2c1ac16e5f9bffa72dacbc6b9e9d454080
Chris Co <coswarm@gmail.com>
2022-12-12 00:49:00 +0300

    ErrorTable + ErrorContent

---
+++ ErrorContent.jsx
@@ -0,0 +1,40 @@
+import { hasListValue } from '@webframer/js'
+import React, { Fragment } from 'react'
+import { ErrorTable } from './ErrorTable.jsx'
+import View from './View.jsx'
+
+/**
+ * Error Alert Message Content - Pure Component.
+ */
+export function ErrorContent ({
+  items,
+  ...props
+}) {
+  // Filter out User vs Server Errors
+  const serverErrors = items.filter(({
+    status,
+    statusCode,
+    code,
+  }) => (((status || statusCode || code) >= 500) || ((status || statusCode || code) == null)))
+  const userErrors = items.filter(({status, statusCode, code}) => ((status || statusCode || code) < 500))
+  return (
+    <Fragment>
+      {hasListValue(serverErrors) &&
+        <View className='border radius'>
+          <ErrorTable className='margin' items={serverErrors} {...props} />
+        </View>
+      }
+      {hasListValue(userErrors) &&
+        <View className={'border radius' + (hasListValue(serverErrors) ? ' margin-top' : '')}>
+          <ErrorTable className='margin' items={userErrors} {...props} />
+        </View>
+      }
+    </Fragment>
+  )
+}
+
+ErrorContent.propTypes = {
+  items: ErrorTable.propTypes.items,
+}
+
+export default React.memo(ErrorContent)
